c000cad42f94facec7a74125a5afd733c10fed94
Tugba Muslu <tugba.evli05@gmail.com>
2023-08-26 14:29:55 +0300

    S17D3 ÖDEVİ

--- src/main/java/com/workintech/zoo/exceptions/AnimalValidator.java
+++ src/main/java/com/workintech/zoo/exceptions/AnimalValidator.java
@@ -2,10 +2,17 @@ package com.workintech.zoo.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.Map;
+
 public class AnimalValidator {
     public static void idIdValid(int id){
         if(id <= 0){
             throw new AnimalException("Id is not valid: " + id, HttpStatus.BAD_REQUEST );
+        }
+    }
+    public static void iSIdNotExist(Map animals, int id){
+        if(!animals.containsKey(id)) {
+            throw new AnimalException("Animal with given id is not exist: " + id, HttpStatus.NOT_FOUND);
         }
     }
 }
